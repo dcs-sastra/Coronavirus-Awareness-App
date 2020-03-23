ba94ab2c958a18f06bfd41bf57b74b78ffa81fbf
Gowtham <48803263+goddamnnoob@users.noreply.github.com>
2020-03-23 13:02:13 +0530

    Progress dialog added

--- app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/home/HomeFragment.java
+++ app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/home/HomeFragment.java
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.database.DataSnapshot;
-
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
