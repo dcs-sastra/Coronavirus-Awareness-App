33bfe1808541bac8e30db8c8e03787a2fdc899be
Goddamnnoob <gowthamreddy547@gmail.com>
2020-04-09 00:02:41 +0530

    final

--- app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/dashboard/DashboardFragment.java
@@ -19,21 +19,11 @@ import dsc.sastra.coronavirus_awareness_app.R;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-      //  dashboardViewModel =
-               // ViewModelProviders.of(this).get(DashboardViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-       /* final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                //textView.setText(s);
-            }
-        });*/
 
         ArrayList<Precautionclass> parray=new ArrayList<Precautionclass>();
 
--- app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/dsc/sastra/coronavirus_awareness_app/ui/notifications/NotificationsFragment.java
@@ -7,46 +7,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
-import androidx.recyclerview.widget.RecyclerView;
 import dsc.sastra.coronavirus_awareness_app.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
     private static final int id=1;
     Countryadapter cadapter;
     private String requrl="";
     ListView lview;
-    ProgressDialog pbar;
+    ProgressDialog pd;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-      //  pbar=new ProgressDialog(this.getActivity());
-       /* final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-
-    /*   pbar.setMessage("Please wait while we are loading...");
-       pbar.setCanceledOnTouchOutside(false);
-       pbar.show();*/
+        pd  = new ProgressDialog(root.getContext());
+        pd.setCancelable(false);
+        pd.setMessage("Loading Please Wait");
        lview=(ListView) root.findViewById(R.id.countrylistview);
        cadapter=new Countryadapter(this.getActivity(),new ArrayList<country>());
        lview.setAdapter(cadapter);
@@ -55,13 +36,17 @@ public class NotificationsFragment extends Fragment {
        requrl=sb.toString();
        ConAsyncTAsk task=new ConAsyncTAsk();
        task.execute(requrl);
-
-   //   pbar.cancel();
         return root;
     }
 
     private class ConAsyncTAsk extends AsyncTask<String, Void, List<country>>
     {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.show();
+        }
+
         @Override
         protected List<country> doInBackground(String... strings) {
             if(strings.length<1|| strings[0]==null)
@@ -75,6 +60,7 @@ public class NotificationsFragment extends Fragment {
         @Override
         protected void onPostExecute(List<country> countries) {
             cadapter.clear();
+            pd.dismiss();
             if(countries!=null && !countries.isEmpty())
             {
                 cadapter.addAll(countries);
